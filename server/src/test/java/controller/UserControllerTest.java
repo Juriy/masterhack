9743b52101b42ab0a503f6fe4e5c47ddb84f6e2c
Juriy Bura <juriy.bura@gmail.com>
2015-03-21 17:11:29 +0800

    Groups tested

--- server/src/test/java/controller/UserControllerTest.java
+++ server/src/test/java/controller/UserControllerTest.java
@@ -1,9 +1,23 @@
 package controller;
 
+import com.google.gson.Gson;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 
 public class UserControllerTest {
 
+    @Test
+    public void testGson(){
+        Gson gson = new Gson();
+        Collection<String> strings = new ArrayList<>();
+        strings.add("user1");
+        strings.add("user2");
+        System.out.println(gson.toJson(strings));
+
+    }
 
 }
