9a1e50adf957f16aef46af8a03e4238bd6da66cb
Juriy Bura <juriy.bura@gmail.com>
2015-03-22 10:16:03 +0800

    Added mom handling

---
+++ client/app/controllers/MomController.js
@@ -0,0 +1,12 @@
+(function() {
+
+    angular
+        .module('masterhack')
+        .controller('MomController', MomController);
+
+    function MomController($scope) {
+        $scope.pot = ''
+
+    }
+
+})();
